07605965ebc857c97af5b9ca31d87a8df4c2b36b
Chhayflyguy <chhayhighlay1232@gmail.com>
2025-01-13 14:07:22 +0700

    laravel

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -2,66 +2,54 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\group;
+use App\Models\Group;
 use Illuminate\Http\Request;
 
 class GroupController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $groups = group::all();
-        return $groups;
+        $groups = Group::all();
+        return view('groups', compact('groups'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
-    {
-       $fields = $request->validate([
-        'name' => 'required|string',
-        'gender' => 'required|string',
-        'email' => 'required|string',
-        'system' => 'required|string',
-       ]);
-
-       $group = group::create($fields);
-       return $group;
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(group $group)
-    {
-        return $group;
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, group $group)
     {
         $fields = $request->validate([
             'name' => 'required|string',
             'gender' => 'required|string',
-            'email' => 'required|string',
+            'email' => 'required|email',
             'system' => 'required|string',
-           ]);
+        ]);
 
-           $group->update($fields);
-           return $group;
+        Group::create($fields);
+
+        return redirect()->route('groups.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(group $group)
+    public function edit(Group $group)
+    {
+        return view('groups.edit', compact('group'));
+    }
+
+    public function update(Request $request, Group $group)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'gender' => 'required|string',
+            'email' => 'required|email',
+            'system' => 'required|string',
+        ]);
+
+        $group->update($fields);
+
+        return redirect()->route('groups.index');
+    }
+
+    public function destroy(Group $group)
     {
         $group->delete();
-        return ['message' => 'Member deleted successfully'];
+
+        return redirect()->route('groups.index');
     }
 }
